331d0fa0c6e5975ab315c9626ffd9a8ad88bdece
wpromare <75404754+wpromare@users.noreply.github.com>
2022-11-01 18:23:29 +0800

    Delete WpromareCore directory

--- WpromareCore/the_first_try.py
+++
@@ -1,6 +0,0 @@
-from cypto.classical_cryptography.caesar_cipher import Caesar_cipher,brute_force_for_Caesar
-a=Caesar_cipher(txt="jgnnq",task="decrypt")
-b=brute_force_for_Caesar(a)
-b.process()
-print(b.get_result_li())
-
